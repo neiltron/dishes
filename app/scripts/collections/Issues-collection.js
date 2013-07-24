484c8e9b06a461b3b014bf55183d6958029ca8d7
Neil Pullman <neil@descend.org>
2013-07-23 23:50:34 -0400

    add token checking and check for etags

--- app/scripts/collections/Issues-collection.js
+++ app/scripts/collections/Issues-collection.js
@@ -15,29 +15,65 @@ define([
         localStorage: new Backbone.LocalStorage('Issues'),
 
         initialize: function () {
+            this.fetch();
+            this.verifyAccessToken();
+        },
+
+        verifyAccessToken: function () {
+            var accessToken = window.localStorage['access_token'] || '',
+                that = this;
+
+            $.ajax({
+                type: 'get',
+                url: 'https://api.github.com/applications/19a40e62322f61bef109/tokens/' + accessToken,
+                success: function (data, status) {
+                    this.fetchFromGithub();
+                },
+                error: function (request) {
+                    that.authenticate();
+                }
+            });
+        },
+
+        authenticate: function () {
             var that = this;
 
-            this.fetch();
-
-            OAuth.initialize('eP7bkHIVjYxvjVHW6YOqQ626_lI');
+            OAuth.initialize('U1c6yltalcdfq2isdYeAe34zZSE');
             OAuth.popup('github', function(error, result) {
-                $.ajax({
-                    url: "https://api.github.com/issues",
-                    dataType: 'json',
-                    data: {
-                        per_page: 100,
-                        filter: 'all',
-                        state: 'open'
-                    },
-                    success: function(data, status, request) {
-                        _.each(data, function (item) {
-                            that.create(item);
-                        });
-                    },
-                    beforeSend: function(xhr, settings) { xhr.setRequestHeader('Authorization','Bearer ' + result.access_token); }
-                });
+                window.localStorage['access_token'] = result.access_token;
+
+                that.fetchFromGithub();
+            });
+        },
+
+        fetchFromGithub: function () {
+            var that = this;
+
+            $.ajax({
+                url: "https://api.github.com/issues",
+                dataType: 'json',
+                data: {
+                    per_page: 100,
+                    filter: 'all',
+                    state: 'open'
+                },
+                success: function(data, status, request) {
+                    window.localStorage['issue_etag'] = request.ETag;
+
+                    _.each(data, function (item) {
+                        that.create(item);
+                    });
+                },
+                beforeSend: function(xhr) {
+                    var accessToken = window.localStorage['access_token'],
+                        issuesETag = window.localStorage['issue_etag'];
+
+                    xhr.setRequestHeader('Authorization','Bearer ' + accessToken);
+                    xhr.setRequestHeader('If-None-Match', issuesETag);
+                }
             });
         }
+
     });
 
     return new IssuesCollection();
